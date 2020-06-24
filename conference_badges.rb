49f68638cae3c5277375617c326a564a54e1966e
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-24 00:56:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_room (speaker)
   mess = []
-speaker.each_with_index{ |speaker, index| mess << "Hello, #{speaker} You'll be assigned to room #{index+1}!"}
+speaker.each_with_index{ |speaker, index| mess << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
 return mess
 end
   
