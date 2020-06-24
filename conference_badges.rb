8147aa37a81875d08b38ad80ea021d04e12883cf
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-24 00:42:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,10 @@ def badge_maker(speaker)
   return "Hello, my name is #{speaker}."
 end
 
-batch_badge_creator(name)
+def batch_badge_creator(name)
   messages = []
     name.each do |name|
       messages << badge_maker(name)
   end
   return messages
+end
